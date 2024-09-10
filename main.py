b7126b464aef9497a245fcacfa820981480b3cce
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-09-10 00:13:30 -0300

    add movable player

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    player = Player(x, y)
+    
+    player.add(updatable, drawable)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color=(0,0,0))
+        
+        screen.fill((0,0,0))
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
